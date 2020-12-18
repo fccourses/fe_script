02c22b82c57b4b7c2bacf3fb95970db65dbb8f4f
Vladimir <freshcode.yt@gmail.com>
2020-12-18 18:28:54 +0200

    Add screen example

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-console.log(1);
+const message = 'Маша сказала: \"Привет\"';
 
-const verdict = false;
-
-if (verdict) {
-  console.log('ok. you are signed up');
-} else {
-  console.log("not ok. you are not signed up");
-}
-
-
-console.log(3);
+console.log( message );
