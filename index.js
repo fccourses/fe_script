a426d503591bfe1f443d0d6574d126f6d3365a10
Vladimir <freshcode.yt@gmail.com>
2020-12-21 16:15:05 +0200

    Add sayHello

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const userInput1 = +prompt("Введите число 1:");
-const userInput2 = +prompt("Введите число 2:");
-
-const getSumOfTwo = function (num1, num2) {
-  //function expression
-  const result = num1 + num2;
-
-  if (!isNaN(result)) {
-    return result;
-  }
-  return false;
+const sayHelloTo = function (name) {
+  alert("Hello, " + name);
 };
 
-const sum = getSumOfTwo(userInput1, userInput2);
+const userName = prompt('Enter your name');
 
-if (!sum) {
-  alert("введите правильно ваши числа");
-} else {
-  console.log(sum);
-}
+sayHelloTo(userName);
