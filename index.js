2a70dfd6e718a1560b739f145e73d94e68d035dc
Vladimir <freshcode.yt@gmail.com>
2020-12-21 15:08:07 +0200

    Add prompt script

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let test = 1,
-  iter = 10,
-  value = 100,
-  number = 1000,
-  string = "test";
+const userInputNumber1 = prompt("Введите число 1:");
+const userInputNumber2 = prompt("Введите число 2:");
 
-console.log();
+const userNumber1 = +userInputNumber1;
+const userNumber2 = +userInputNumber2;
+
+const result = userNumber1 + userNumber2;
+
+const verdict = isNaN(result);
+
+if (!verdict) {
+  alert("Сумма чисел: " + result);
+} else {
+  alert("Вы ввели не число!");
+}
