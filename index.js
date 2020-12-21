22d00748b942979049434719dc76369edf3545ab
Vladimir <freshcode.yt@gmail.com>
2020-12-21 15:20:20 +0200

    Add function expression

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const userInputNumber1 = prompt("Введите число 1:");
-const userInputNumber2 = prompt("Введите число 2:");
+const getSumOfTwo = function () { //function expression
 
-const userNumber1 = +userInputNumber1;
-const userNumber2 = +userInputNumber2;
+  const userInputNumber1 = +prompt("Введите число 1:");
+  const userInputNumber2 = +prompt("Введите число 2:");
 
-const result = userNumber1 + userNumber2;
+  const result = userInputNumber1 + userInputNumber2;
 
-const verdict = isNaN(result);
+  const verdict = isNaN(result);
 
-if (!verdict) {
-  alert("Сумма чисел: " + result);
-} else {
-  alert("Вы ввели не число!");
-}
+  if (!verdict) {
+    alert("Сумма чисел: " + result);
+  } else {
+    alert("Вы ввели не число!");
+  }
+  
+};
+
+getSumOfTwo();
