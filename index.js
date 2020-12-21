e76b8d77093657f06a8ec187c9cc774e967a9e88
Vladimir <freshcode.yt@gmail.com>
2020-12-21 16:11:13 +0200

    Add sum func 2

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const userInput2 = +prompt("Введите число 2:");
 
 const getSumOfTwo = function (num1, num2) {
   //function expression
-  return num1 + num2;
+  const result = num1 + num2;
+
+  if (!isNaN(result)) {
+    return result;
+  }
+  return false;
 };
 
 const sum = getSumOfTwo(userInput1, userInput2);
 
-console.log(sum);
+if (!sum) {
+  alert("введите правильно ваши числа");
+} else {
+  console.log(sum);
+}
