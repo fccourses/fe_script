a3205f521a1c84d8b98bd71dad71b7652e804596
Vladimir <freshcode.yt@gmail.com>
2020-12-21 17:03:14 +0200

    Add 3 tasks

--- index.js
+++ index.js
@@ -4,7 +4,32 @@ const sayHelloTo = function (name) {
   // После return а функция не работает.
 };
 
-const userName = prompt('Enter your name');
+const userName = prompt("Enter your name");
 
 const box = sayHelloTo(userName);
 console.log(box);
+
+/* 
+1. Функция, которая возвращает минимальное значение.
+2. Функция, которая возвращает максимальное значение.
+3. Функция, которая проверяет чётность числа.
+(*4. Которая решает квадратные уравнения.)
+*/
+
+const getMinValue = function (a, b) {
+  if (a > b) {
+    return b;
+  }
+  return a;
+};
+
+const getMaxValue = function (a, b) {
+  if (a < b) {
+    return b;
+  }
+  return a;
+};
+
+const isEven = function (number) {
+  return number % 2 === 0;
+}
