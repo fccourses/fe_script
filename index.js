0a26536a374e0d7e7269835312cb6190b17dd5d5
Vladimir <freshcode.yt@gmail.com>
2020-12-22 18:37:05 +0200

    Add scope

--- index.js
+++ index.js
@@ -1,66 +1,22 @@
-const sayHelloTo = function (name) {
-  alert('Hello, ' + name);
-  return; // Завершение работы функции.
-  // После return а функция не работает.
-};
+const globalVar = 'global';
 
-// const userName = prompt('Enter your name');
+if (globalVar) {
+  const ifVar1 = 1;
 
-// const box = sayHelloTo(userName);
-// console.log(box);
+  console.log(ifVar1);
+  console.log(globalVar);
 
-/* 
-1. Функция, которая возвращает минимальное значение.
-2. Функция, которая возвращает максимальное значение.
-3. Функция, которая проверяет чётность числа.
-(*4. Которая решает квадратные уравнения.)
-*/
+  if (globalVar) {
+    const ifVar2 = 'Test';
 
-const getMinValue = function (a, b) {
-  if (a > b) {
-    return b;
-  }
-  return a;
-};
-
-const getMaxValue = function (a, b) {
-  if (a < b) {
-    return b;
-  }
-  return a;
-};
-
-const isEven = function (number) {
-  return number % 2 === 0;
-};
-
-/**
- * Solve the square equation
- * @param {number} a - first coef
- * @param {number} b - second coef
- * @param {number} c - third coef
- * @returns { boolean | null | number[] } - roots of square eq or error
- */
-const solveSquareEquation = function (a, b, c) {
-  if (isNaN(a - b - c)) {
-    return null;
-  }
-  const d = b * b - 4 * a * c;
-
-  if (d < 0) {
-    return false;
+    console.log(ifVar1);
+    console.log(ifVar2);
+    console.log(globalVar);
   }
 
-  if (d === 0) {
-    const x = -b / (2 * a);
-    return [x, x];
-  }
+  // console.log(ifVar2); // error
+}
+// console.log(ifVar2); // error
 
-  if (d > 0) {
-    const dSqrt = d ** (1 / 2);
-    const x1 = (-b + dSqrt) / (2 * a);
-    const x2 = (-b - dSqrt) / (2 * a);
-
-    return [x1, x2];
-  }
-};
+// console.log(ifVar);
+console.log('end of script');
