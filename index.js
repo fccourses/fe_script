c207bfaa8b4a8b5105d3aa0a287a4cbd3eef6c10
Vladimir <freshcode.yt@gmail.com>
2020-12-23 20:22:02 +0200

    Add for loop

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
    пока он не введёт правильный
    1.1 Ограничить кол-во попыток до 5
 */
+
 let count = 0;
-while (false) {
+while (count < MAX_TRY) {
+  count++;
   const userInput = prompt('Pass here');
 
   if (userInput === GOOD_PASSWORD) {
@@ -13,9 +15,12 @@ while (false) {
   }
 }
 
-do {
-  const teset = 'test';
-  console.log(teset);
-} while (false);
+for (let count = 0; count < MAX_TRY; count++) {
+  const userInput = prompt('Pass here');
+
+  if (userInput === GOOD_PASSWORD) {
+    break;
+  }
+}
 
 /* 2. Найти факториал числа */
