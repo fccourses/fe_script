65862dc00919761b0e2b26e014a5f60325150189
Vladimir <freshcode.yt@gmail.com>
2020-12-23 18:29:22 +0200

    Add func declaration

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-/* const sum = function (a, b) {
+console.log(sum2(5, 5));
 
-
-
+const sum1 = function (a, b) {
+  //function expression
   return a + b;
 };
+
 const sub = function (a, b) {
   return a - b;
 };
+
 const mul = function (a, b) {
   return a * b;
 };
+
 const div = function (a, b) {
   return a / b;
 };
 
-
- */
-
-const getUserInput = function () {
-  const input = prompt('Введите что-нибудь');
-
-  /*  let returnValue;
-  if (input) {
-    returnValue = 'Вы ввели: ' + input;
-  } else {
-    returnValue = 'Вы ничего не ввели';
-  } */
-
-  const returnValue = input ? 'Вы ввели: ' + input : 'Вы ничего не ввели';
-
-  return returnValue;
-};
-const res = getUserInput();
-
-console.log(res);
+function sum2(a, b) {
+  // function declaration
+  return a + b;
+}
