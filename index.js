e863cfb4b5b3e66fe45673788a6c56a62cd25429
Vladimir <freshcode.yt@gmail.com>
2020-12-23 18:54:41 +0200

    Kill function purity

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-console.log(sum(3, 7));
+let global = 1;
+
+console.log(square(3, 7));
+
+global = 10;
+
+console.log(square(3, 7));
 
 function sub(a, b) {
   return a - b;
 }
 
-function mul(a, b) {// pure func
+function mul(a, b) {
+  // pure func
   return a * b;
 }
 
@@ -18,7 +25,11 @@ function sum(a, b) {
   return a + b;
 }
 
-function square(num) {// pure func
+function square(num) {
+  // pure func
   const result = mul(num, num);
-  return result;
+
+  console.log('1');
+
+  return result + global;
 }
