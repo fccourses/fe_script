70ab4f9bc0a58a58fc061661d24810e570461850
Vladimir <freshcode.yt@gmail.com>
2020-12-23 18:22:47 +0200

    Add more exmple

--- index.js
+++ index.js
@@ -18,9 +18,19 @@ const div = function (a, b) {
  */
 
 const getUserInput = function () {
-  const result = prompt('Введите что-нибудь');
-  return result;
+  const input = prompt('Введите что-нибудь');
+
+  /*  let returnValue;
+  if (input) {
+    returnValue = 'Вы ввели: ' + input;
+  } else {
+    returnValue = 'Вы ничего не ввели';
+  } */
+
+  const returnValue = input ? 'Вы ввели: ' + input : 'Вы ничего не ввели';
+
+  return returnValue;
 };
 const res = getUserInput();
 
-console.log(res ? 'Вы ввели: ' + res : 'Вы ничего не ввели');
+console.log(res);
