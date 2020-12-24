2bfa3e187682714b612430f8d947e27ace2d6749
Vladimir <freshcode.yt@gmail.com>
2020-12-24 18:55:23 +0200

    Add CRUD for objects

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const cat = {}; // литерал
-const cat2 = Object();
-const cat3 = new Object();
+const cat = { //C
+  name: 'Pushok',
+  color: 'red',
+  breed: 'Persian',
+  age: 5,
+  isSleeping: true,
+  isMale: true,
+};
+
+console.log(cat.age); //R
+
+cat.age++; //U
+cat.age = cat.age + 1;
+
+console.log(cat);
+
+cat.isSleeping = undefined; //bad delete
+
+delete cat.isSleeping; //D
