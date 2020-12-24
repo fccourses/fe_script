d08b11d1b322a8cbd93f78b1cba30622823ced86
Vladimir <freshcode.yt@gmail.com>
2020-12-24 18:12:45 +0200

    Add factorial

--- constants.js
+++ constants.js
@@ -1,2 +1,3 @@
 const GOOD_PASSWORD = 'qwerty123';
 const MAX_TRY = 5;
+const MAX_SAFE_FACTORIAL = 250;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
    пока он не введёт правильный
    1.1 Ограничить кол-во попыток до 5
 */
-
+/* 
 let count = 0;
 while (count < MAX_TRY) {
   count++;
@@ -21,6 +21,31 @@ for (let count = 0; count < MAX_TRY; count++) {
   if (userInput === GOOD_PASSWORD) {
     break;
   }
+} */
+
+/*          Функции:                    */
+/* 2. Найти факториал числа */
+
+function factorial(number) {
+  if (number < 0) {
+    return null;
+  }
+
+  if (number === 0) {
+    return 1;
+  }
+
+  if (number > MAX_SAFE_FACTORIAL) {
+    return false;
+  }
+
+  let result = 1n;
+
+  for (let i = 1; i <= number; i++) {
+    result *= BigInt(i);
+  }
+
+  return result;
 }
 
-/* 2. Найти факториал числа */
+/* 3. Возвести число в заданную степень(power) */
