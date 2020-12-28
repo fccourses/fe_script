2dcc00925157b25a9282be176f103f1f422c0224
Vladimir <freshcode.yt@gmail.com>
2020-12-28 20:36:24 +0200

    Add createMultiplicationTable

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 'use strict';
 
-const user = {
-  'favorite color': 'red',
-  0: 'test value',
-  2: 'value unique',
-};
+function createMultiplicationTable() {
+  const table = {};
+  debugger;
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      table[`${i} * ${j} = `] = i * j;
+    }
+  }
+
+  return table;
+}
 
 
-/* Прочитали существующие свойства */
-console.log(user['0']);
-console.log(user['2']);
-console.log(user[0]);
-console.log(user[2]);
-console.log(user['favorite color']);
 
-const test = 'favorite color';
-user[test];
-console.log(user[test]);
-
-
-/* Создали новое свойство */
-const valueFromUser = 'tset';
-user[valueFromUser] = 3;
