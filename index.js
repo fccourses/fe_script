2619f33b86979247be2d36d9c20f26e17f204cc9
Vladimir <freshcode.yt@gmail.com>
2020-12-28 20:47:24 +0200

    Add getQuarter

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function createMultiplicationTable() {
+function createMultiplicationTable(limit) {
   const table = {};
   debugger;
-  for (let i = 1; i < 10; i++) {
-    for (let j = 1; j < 10; j++) {
+  for (let i = 1; i < limit; i++) {
+    for (let j = 1; j < limit; j++) {
       table[`${i} * ${j} = `] = i * j;
     }
   }
@@ -12,5 +12,18 @@ function createMultiplicationTable() {
   return table;
 }
 
+/*  */
 
+/* Определить квартал года по текущему месяцу(1-12) */
 
+function getQuarter(monthNumber) {
+  return Math.ceil(monthNumber / 3);
+}
+
+/* 
+    Написать функцию:
+    Создать объект семья
+    Ключём будет имя родстенника
+    А значением будет информация о нём
+    Объект создавать через prompt
+*/
