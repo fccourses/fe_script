56b76e2d598c076180a3df812628b838e164fdf7
Vladimir <freshcode.yt@gmail.com>
2020-12-29 18:16:21 +0200

    Add some example

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const obj1 = {};
 const obj2 = Object();
 
 function MyObj() {
+  // new - создаёт пустой объект для функции конструктора.
+  // новосозданный объект - контекст( this )
   if (!new.target) {
     return new MyObj();
   }
+
+  this.prop = 'value';
+
 }
 
 console.log(MyObj());
