9a7d89008ed184a2600cb44882b3fbd7d18a6d6e
Vladimir <freshcode.yt@gmail.com>
2020-12-29 20:48:59 +0200

    Add Accumulator

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ function MyArrayProto() {
 }
 
 /* Объекты с данными */
-function MyArray() {
+function MyArray(...args) {
   this.length = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    this.push(arguments[i]);
+  }
 }
 
 /* Создаём прототип(связь между объектами). Наследование */
@@ -33,4 +37,41 @@ const arr = new Array(1, 2, 3, 4, 5);
 /* 
 myArr.push(1);
 myArr.push(2);
-myArr.push(3); */
+myArr.push(3); 
+*/
+
+/*
+Напишите функцию-конструктор Accumulator(startingValue).
+
+Объект, который она создаёт, должен уметь следующее:
+Хранить «текущее значение» в свойстве value. 
+Начальное значение устанавливается в аргументе конструктора startingValue.
+
+Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+Таким образом, свойство value является текущей суммой всего,
+что ввёл пользователь при вызовах метода read(),
+с учётом начального значения startingValue. 
+*/
+
+function Accumulator(startingValue) {
+  if (!this.checkNumber(startingValue)) {
+    this.value = startingValue;
+  }
+}
+
+function AccumulatorProto() {
+  this.read = function read() {
+    const userNumber = +prompt('Введите число');
+    if (this.checkNumber(userNumber)) {
+      return;
+    }
+    this.value += userNumber;
+    return this.value;
+  };
+
+  this.checkNumber = function checkNumber(number) {
+    return isNaN(number) && number > 0;
+  };
+}
+
+Accumulator.prototype = new AccumulatorProto();
