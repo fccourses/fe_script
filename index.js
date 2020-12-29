55ec53f612621b24c127f1ef4e4fcde618c1ae56
Vladimir <freshcode.yt@gmail.com>
2020-12-29 18:15:01 +0200

    Add "new" key word

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 'use strict';
 
-const t1 = 1;
-const t2 = 1;
+const obj1 = {};
 
-const obj1 = {
-  test: 1,
-};
+const obj2 = Object();
 
-const obj2 = {
-  test: 1,
-};
+function MyObj() {
+  if (!new.target) {
+    return new MyObj();
+  }
+}
 
-const link1 = obj1; // копирование по ссылке
-
-console.log(link1 === obj1); //true
-console.log(link1 === obj2); //false
-
-link1.test2 = 'str';
-
-console.log(link1);
-console.log(obj1);
+console.log(MyObj());
