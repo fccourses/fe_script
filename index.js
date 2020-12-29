2acf93f9930599b926b970d7c51aa9d889068292
Vladimir <freshcode.yt@gmail.com>
2020-12-29 19:28:06 +0200

    Add MyArray

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const obj1 = {};
+const arr = ['4807539845', '2435y237', '23452435'];
+const arrTels = new Array('4807539845', '2435y237', '23452435');
 
-const obj2 = Object();
-
-function MyObj() {
-  // new - создаёт пустой объект для функции конструктора.
-  // новосозданный объект - контекст( this )
-  if (!new.target) {
-    return new MyObj();
-  }
-
-  this.prop = 'value';
-
+/* Объект с логикой */
+function MyArrayProto() {
+  this.push = function push(item) {
+    this[this.length] = item;
+    return ++this.length;
+  };
 }
 
-console.log(MyObj());
+/* Объекты с данными */
+function MyArray() {
+  this.length = 0;
+}
+
+/* Создаём прототип(связь между объектами). Наследование */
+MyArray.prototype = new MyArrayProto();
+
+const myArr = new MyArray(); // 1
+const myArr2 = new MyArray(); // 2
+
+arr.push(1); // Откуда берётся метод push() ?
