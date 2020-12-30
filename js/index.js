e5dadafcc3c234458a4ff6e2119f714af81f90ac
Vladimir <freshcode.yt@gmail.com>
2020-12-30 18:37:06 +0200

    Add __proto__

--- js/index.js
+++ js/index.js
@@ -1,33 +1,17 @@
 'use strict';
 
-function Student(firstName, lastName, age, isMale) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-}
+const car = {
+  go() {
+    return `${this.name} сейчас за рулём!`;
+  },
+};
 
-function StudentPrototype() {
-  this.toString = function toString() {
-    return `${this.firstName} ${this.lastName}`;
-  };
-  this.sayHello = function sayHello() {
-    return `${this.firstName} ${this.lastName} says "Hello!"`;
-  };
-  this.say = function say(string) {
-    return `Студент ${this.firstName} ${this.lastName} говорит:\n${string}`;
-  };
-  /* 
-  say() метод принимающий строку и возвращающий строку 
-  в таком формате:
-  `Студент <Имя студента> говорит:
-   <содержимое принятой строки>`  
-  */
-}
+const driver1 = {
+  name: 'Test Testovich',
+};
+const driver2 = {
+  name: 'Tesan Tesovich',
+};
 
-const studentPrototypeObject = new StudentPrototype(); // создаём объект, который будет прототипом для всех объектов созданных через new Student()
-Student.prototype = studentPrototypeObject;
-
-
-const stud1 = new Student('Vlad', 'Test', 3, true);
-const stud2 = new Student('12', '22', 32, false);
+driver1.__proto__ = car;
+driver2.__proto__ = car;
