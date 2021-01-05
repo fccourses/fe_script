f08ea2998a9e24cfa9638bfff01c0252510e8f73
Vladimir <freshcode.yt@gmail.com>
2021-01-05 20:01:53 +0200

    Add createRandomUsers

--- js/index.js
+++ js/index.js
@@ -1,63 +1,32 @@
 'use strict';
-
-const nums = [
-  1,
-  3,
-  5,
-  7,
-  10,
-  23,
-  4,
-  325,
-  23534,
-  45,
-  7658,
-  67,
-  978,
-  90,
-  7357,
-  24,
-  345,
-  345,
-  435,
-  345,
-  435,
-];
-const users = [{}, {}, {}];
-/* 
-function square(currentNumber) {
-  console.log(currentNumber * currentNumber);
-  return currentNumber * currentNumber;
+function getFullName() {
+  return `${this.firstName} ${this.lastName}`;
 }
- */
-function addSubscribe(user) {
-  user.isSubscribed = true;
+function User(first, last, age, email, isMale, isSubscribed = false) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.email = email;
+  this.isMale = isMale;
+  this.isSubscribed = isSubscribed;
+  this.getFullName = getFullName;
 }
-
-users.forEach(addSubscribe);
-
-// const myArr = new MyArray(1, 2, 3, 4, 5);
-//=========SOME===========================
-
-function isEven(number) {
-  return number % 2 === 0;
+function createRandomUsers(amount = 1) {
+  const db = [];
+  for (let i = 0; i < amount; i++) {
+    const user = new User(
+      `Name${i}`, // first name
+      `Surname${i}`, //last name
+      Math.ceil(Math.random() * 100), //age
+      `email${i}@mail.com`, //email
+      Math.random() > 0.5 //isMale
+    );
+    db.push(user);
+  }
+  return db;
 }
+const users = createRandomUsers(100);
+console.table(users);
 
-const res = nums.some(isEven);
-console.log(res);
-
-const myArr = new MyArray(2, 2, 2, 2, 1, 3, 5);
-
-console.log(myArr.some(isEven));
-
-// ===========EVERY===========
-
-console.log(myArr.every(isEven)); //false
-
-//============FILTER============
-
-function isOdd(num) {
-  return num % 2 !== 0;
-}
-
-const filteredNumberArray = nums.filter(isOdd);
+/* Получить массив полных имён с помощью метода map */
+/* ПОлучить массив совершеннолетних пользователей */
