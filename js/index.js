707a7bbdf1ca0b583127de3017d857d4dca38ae4
Vladimir <freshcode.yt@gmail.com>
2021-01-06 18:38:45 +0200

    Add bind example

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 'use strict';
+function logItem(currentArticle, index) {
+  console.log(`${this.title} => ${index + 1}: ${currentArticle}`);
+}
 
 const newsPaper = {
   title: 'FC daily',
   articles: ['Test1', 'Heading1', 'String1', 'Article1', 'INTERESTING'],
 
   showArticles() {
-    this.articles.forEach((currentArticle, index) => {
-      // console.log(this)
-      console.log(`${this.title} => ${index + 1}: ${currentArticle}`);
-    });
+    this.articles.forEach(logItem.bind(this));//v 2
   },
 };
 
