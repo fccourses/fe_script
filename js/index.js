7f94c2854ecd979342df43f985eed79830d9c716
Vladimir <freshcode.yt@gmail.com>
2021-01-06 19:24:27 +0200

    Add sum

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
 'use strict';
 
-const t1 = function () {
-  console.log(arguments);
-};
-
-const t2 = () => {
-  console.log(arguments); // error. arguments is not defined
-};
-
-t1(1, 1, 1, 1);
-
-// t2(1, 1, 1, 1);
-
-function sum() {
-  let res = null;
-
-  for (let i = 0; i < arguments.length; i++) {
-    res += arguments[i];
-  }
-
-  return res;
+function sum(a, b, ...rest) {
+  console.log(rest);
 }
+
+const sumArrow = (...rest) => {
+  let accumulator = null;
+
+  rest.forEach((number) => {
+    accumulator += number;
+  });
+
+  return accumulator;
+};
