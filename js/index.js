afa85319c8521328cb1c2cbb19658b4516576b70
Vladimir <freshcode.yt@gmail.com>
2021-01-06 19:39:34 +0200

    Add arr concat without concat

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 const numbers = [1, 1, 32, 112, 1243, 32, 4];
 const numbers2 = [1234, 1324, 43, 4356, 4];
 
+const arr = [...numbers, ...numbers2];
+
 const sumArrowFE = (...rest) => { //rest operator
   let accumulator = null;
 
