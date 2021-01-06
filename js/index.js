c4847036ac07896e80161e42e41a7b3c7ba7f42c
Vladimir <freshcode.yt@gmail.com>
2021-01-06 19:58:32 +0200

    Add recursion

--- js/index.js
+++ js/index.js
@@ -1,25 +1,9 @@
 'use strict';
 
-const numbers = [1, 1, 32, 112, 1243, 32, 4];
-const numbers2 = [1234, 1324, 43, 4356, 4];
-
-const arr = [...numbers, ...numbers2];
-
-const sumArrowFE = (...rest) => { //rest operator
-  let accumulator = null;
-
-  rest.forEach((number) => {
-    accumulator += number;
-  });
-
-  return accumulator;
-};
-
-const sumReduce = (...rest) => { //rest operator
-  return rest.reduce((accumulator, value) => {
-    return accumulator + value;
-  });
-};
-
-sumArrowFE(...numbers); //spread operator
-sumReduce(...numbers); //spread operator
+function countDown(num) {
+  console.log(num);
+  if (num > 0) {
+    countDown(num - 1); // recursion
+  }
+  return true; //basis
+}
