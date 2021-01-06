360b0227aa52b0c83d417979d73d9d4285f92c07
Vladimir <freshcode.yt@gmail.com>
2021-01-06 20:44:10 +0200

    Add parenthsis

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 'use strict';
-
+/* 1. Возможность отрицательных степеней */
 const power = (num, exp) => {
-  debugger;
-  if (exp <= 1) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp === 1) {
     return num; //basis
   }
-  return num * power(num, exp - 1); //recursion
+  return exp > 0
+    ? num * power(num, exp - 1)
+    : 1 / (num * power(num, Math.abs(exp) - 1));
 };
 
 /* 
@@ -15,3 +19,16 @@ const power = (num, exp) => {
   2) Доходим до базы
   3) Функции начнут завершать свою работу
 */
+
+/* 2. Факториал */
+
+const factorial = (n) => (n <= 0 ? 1 : n * factorial(n - 1));
+
+/* 3. 
+par(3) // '((()))'
+par(2) // '(())'
+*/
+
+const par = (n) => (n <= 1 ? '()' : `(${par(n - 1)})`);
+
+/* customFlat() || MyArray.prototype.flat() */
