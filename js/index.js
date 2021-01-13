97b88a586a16dacf8813d228c38d5358ddc4380a
Vladimir <freshcode.yt@gmail.com>
2021-01-13 18:54:33 +0200

    Add inheritance

--- js/index.js
+++ js/index.js
@@ -1,2 +1,41 @@
-'use strict';
+class User {
+  constructor(name, surname, age, isBanned) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.isBanned = isBanned;
+  }
 
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
+
+/* Admin -  у него есть всё, что у User
+    ban(user);
+*/
+
+class Admin extends User {
+  constructor(name, surname, age, email) {
+    super(name, surname, age, false);
+    this.email = email;
+  }
+
+  ban(user) {
+    user.isBanned = true;
+  }
+
+  unBan(user) {
+    user.isBanned = false;
+  }
+
+  toggleBan(user) {
+    if (user instanceof User) {
+      user.isBanned = !user.isBanned;
+      return;
+    }
+    throw new TypeError();
+  }
+}
+const u = new User();
+const a = new Admin();
