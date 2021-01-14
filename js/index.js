570ee99dd84ce9bd01abc84c724d25af49fb39f1
Vladimir <freshcode.yt@gmail.com>
2021-01-14 19:46:42 +0200

    for..of

--- js/index.js
+++ js/index.js
@@ -1,22 +1,19 @@
+const arr = [1, 2, 3, 4, 5, 6];
+
+for (const number of arr) {
+  console.log(number);
+}
+
+console.log(...arr);
+
+const myArr = new MyArray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+
 /* 
+Недоступно:
 
-1. string - ''
-2. number - NaN, numebers, Infiniti, - Infinity, -0, +0
-3. bool - true, false
-4. symbol - ???
-5. undefined - undefined
-6. null - null
-7. BigInt - 100n
+console.log(...myArr);
 
-8. Object - {*}
-
-*/
-
-const mySymbol = Symbol('Symbol.Important');
-const mySymbol2 = Symbol('Просто метка для людей.');
-
-const object = {
-  login: 'Vasy12345',
-  test: 'wrong way',
-  [mySymbol]: 100,
-};
+for (const num of myArr) {
+  console.log(num);
+} */
