9f0975f1268b9227c15fc56f7c57001dbcdf39b6
Vladimir <freshcode.yt@gmail.com>
2021-01-19 19:55:57 +0200

    Add Queue task solution

--- js/Queue.js
+++ js/Queue.js
@@ -1,7 +1,11 @@
 class Queue {
-  constructor() {
+  constructor(...args) {
     this._head = 0;
     this._tail = 0;
+
+    for (const item of args) {
+      this.push(item);
+    }
   }
 
   get size() {
@@ -23,4 +27,28 @@ class Queue {
   }
 }
 
-const q = new Queue();
+/* Easy level: Сделать такое создание очереди возможным */
+const q1 = new Queue(1, 3, 5, 7, 9, 11, 13, 15);
+const q2 = new Queue(2, 4, 6, 8);
+
+/*
+Super-mega-pro-advanced level:
+mergeQueues(q1,q2)
+Функция, которая сливает две очереди в одну.
+Элементы из очередей становятся в новую очередь поочерёдно.
+*/
+
+const mergeQueues = (q1, q2) => {
+  const result = new Queue();
+
+  while (q1.size || q2.size) {
+    if (q1.size) {
+      result.push(q1.pop());
+    }
+    if (q2.size) {
+      result.push(q2.pop());
+    }
+  }
+
+  return result;
+};
