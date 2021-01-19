89a65276dd3ddd6443d64d40c0e9e79ff67ff4eb
Vladimir <freshcode.yt@gmail.com>
2021-01-19 20:11:05 +0200

    Add Map example

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
+'use strict';
 
+const mySymbol = Symbol();
 
+const obj = {
+  test: 'test',
+  mySymbol: true,
+};
+
+const myMap = new Map();
+myMap.set(1, {});
+myMap.set('1', true);
+myMap.set(1, 10000);
+myMap.get(1);
+
+myMap.set(obj, 'SUPER IMPORTANT VALUE');
