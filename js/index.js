3b939aec64bfa290085d1502c1bf7b0f2f51834b
Vladimir <freshcode.yt@gmail.com>
2021-01-20 20:24:56 +0200

    Add nested destructure

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 // Деструктуризация - особый способ создания переменных
 
+// Деструктуризация объектов
 const monitor = {
   sizes: {
     height: {
@@ -13,15 +14,19 @@ const monitor = {
       scale: 'cm',
     },
   },
-  model: 'Samsung',
+  model: {
+    company: 'Samsung',
+    type: 'S24SD300M1',
+  },
   brightness: 200,
   contrast: 100,
   color: 'black',
   dpi: 250,
   resolution: '4K',
+
 };
 
-console.log(monitor.sizes.height.value);
+// console.log(monitor.sizes.height.value);
 
 function getDiagonal(monitor) {
   monitor.sizes.height.value;
@@ -30,4 +35,16 @@ function getDiagonal(monitor) {
 
 // const monitorBright = monitor.brightness;
 
-const { brightness, dpi, color } = monitor;
+const { resolution, contrast, model } = monitor;
+
+/* BAD!
+const { sizes } = monitor;
+const { height, width} = sizes;
+const {value} = height */
+
+const {
+  sizes: {
+    height: { value: heightValue },
+    width: { value: widthValue },
+  },
+} = monitor;
